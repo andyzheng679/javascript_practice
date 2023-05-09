c7948fc0671291c36ca7d9edba97f496a5cb7411
Andy Zheng <andyzheng679@gmail.com>
2023-05-08 21:12:26 -0400

    variables

--- index.js
+++ index.js
@@ -9,3 +9,38 @@ window.alert("HELLO!");
 /*
 this is a comment block
 */
+
+//------------------------------------------------------
+/*
+//A variable is a container for storing data
+//a variable behaves as if it was the value that it contains
+
+//two steps:
+//1. declaration (var, let, const)
+//2. assignment (= assignment operator)
+
+let age; 
+age = 77;
+//or
+let age2 = 100; //number
+age2 = age2 - 50
+
+let firstName = "Andy"; //string
+
+let student = true; //booleans
+
+
+console.log(age);
+console.log(age2);
+console.log("Hello", firstName);
+console.log(student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "Hello, you are " + age;
+document.getElementById("p3").innerHTML = "Hello " + student;
+*/
+
+
+
+
+
