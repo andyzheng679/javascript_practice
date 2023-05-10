11972aa772762658dcc4d3324e0d3e624c9a8785
Andy Zheng <andyzheng679@gmail.com>
2023-05-10 14:04:35 -0400

    getting user input

--- index.js
+++ index.js
@@ -64,5 +64,27 @@ console.log(students);
 console.log(extraStudents);
 */
 
+//-----------------------------------------------------------
+
+/*
+// how to accept user input
+
+// easy way with a window prompt 
+
+//let username = window.prompt("What's your name?");
+//console.log(username);
+
+// difficult way HTML textbox
+let username;
+
+document.getElementById("myButton").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = "Hello " + username;
+
+}
+*/
+
+
 
 
