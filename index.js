1987f03de5568f3999ccd49eb10f8a5cfc72ec94
Andy Zheng <andyzheng679@gmail.com>
2023-05-10 14:45:05 -0400

    Math

--- index.js
+++ index.js
@@ -126,8 +126,29 @@ circumference = 2 * PI * radius;
 console.log("the circumference is:", circumference);
 */
 
+//-------------------------------------
+//math
+/*
+let x;
+let y = 5;
+let z = 9;
+let max;
+let min;
 
 
+//x = Math.round(x);
+//x = Math.floor(x);
+//x = Math.ceil(x);
+//x = Math.pow(x, 2);
+//x = Math.sqrt(x);
+//x = Math.abs(x);
+//max = Math.max(x, y, z);
+//min = Math.min(x, y, z);
+x = Math.PI;
+
+
+console.log(x);
+*/
 
 
 
