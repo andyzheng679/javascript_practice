880b23f0d9fd83322e29453f5159cdcc37ecd533
Andy Zheng <andyzheng679@gmail.com>
2023-05-10 14:32:07 -0400

    type conversion

--- index.js
+++ index.js
@@ -85,6 +85,33 @@ document.getElementById("myButton").onclick = function(){
 }
 */
 
+// --------------------------------------
+// type conversion = change the datatype of a value to another
+//                              (strings, numbers, booleans)
+/*
+let age = window.prompt("how old are you?");
+console.log(typeof age);
+age = Number(age);
+console.log(typeof age);
+age += 1;
+
+console.log("Happy Birthday! You are", age, "years old");
+*/
+
+let x;
+let y;
+let z;
+
+x = Number("3.14");
+y = String(3.14);
+z = Boolean("sdf");  //empty = false, anything in it is true
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+
+
 
 
 
