5ac17135adc0abd5ee1be0435ec15b6974750cf4
Andy Zheng <andyzheng679@gmail.com>
2023-05-10 15:08:38 -0400

    counter program

--- index.js
+++ index.js
@@ -183,7 +183,26 @@ document.getElementById("submitButton").onclick = function(){
 }
 */
 
+//------------------------------------------
+//counter program
+/*
+let count = 0;
 
+document.getElementById("decreaseBtn").onclick = function(){
+    count -= 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("resetBtn").onclick = function(){
+    count = 0;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("increaseBtn").onclick = function(){
+    count += 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+*/
 
 
 
