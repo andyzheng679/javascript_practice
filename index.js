4aa113b216868fbbee3da3168ce59d7d8fc5038e
Andy Zheng <andyzheng679@gmail.com>
2023-05-10 19:05:49 -0400

    check property

--- index.js
+++ index.js
@@ -290,6 +290,41 @@ else{
 */
 
 
+//--------------------------------------------------------------
+//check property - if a check box or radio button is selected 
+/*
+document.getElementById("myButton").onclick = function(){
+
+    const myCheckBox = document.getElementById("myCheckBox")
+    const visaBtn = document.getElementById("visaBtn")
+    const mastercardBtn = document.getElementById("mastercardBtn")
+    const paypalBtn = document.getElementById("paypalBtn")
+
+
+    if(myCheckBox.checked){
+        console.log("you're subscribed");
+    }
+    else{
+        console.log("you're not subscribed");
+    }
+
+    if(visaBtn.checked){
+        console.log("You're paying with Visa");
+    }
+    else if(mastercardBtn.checked){
+        console.log("You're paying with MasterCard");
+    }
+    else if(paypalBtn.checked){
+        console.log("You're paying with PayPal");
+    }
+    else{
+        console.log("Select a payment type");
+    }
+}
+*/
+
+
+
 
 
 
